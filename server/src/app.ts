47f724a40d649403802086d76c1ef78e9f8191a3
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-09 14:17:15 +0700

    feat: connect to database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import config from "config";
+import connect from "./utils/connect";
 
 const app = express();
+const port = config.get<number>("port");
 
-app.listen(1337, () => {
+app.listen(port, async () => {
   console.log("App is running!");
+  await connect();
 });
