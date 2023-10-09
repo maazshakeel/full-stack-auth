9ea6754e75410662f0d822d793bcfa3e2892c618
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-09 14:25:14 +0700

    feat: use logger for logging the error or other info

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const app = express();
 const port = config.get<number>("port");
 
 app.listen(port, async () => {
-  console.log("App is running!");
+  logger.info(`App is running at http://localhost:${port}/`);
   await connect();
 });
