78a5c54a766aac866025bc68ef215aba736ac8a7
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-12 10:00:33 +0700

    feat: readme and route new structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,10 +7,10 @@ import routes from "./routes";
 const app = express();
 const port = config.get<number>("port");
 
+app.use(routes);
+
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}/`);
 
   await connect();
-
-  routes(app);
 });
